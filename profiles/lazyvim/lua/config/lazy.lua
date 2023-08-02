e4f68250f2b67095b426f1b5efff4d451899168a
liszt21 <liszt21@qq.com>
2023-08-02 20:53:35 +0800

    feat: update plugins

--- profiles/lazyvim/lua/config/lazy.lua
+++ profiles/lazyvim/lua/config/lazy.lua
@@ -24,7 +24,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.test.core" },
-    { import = "lazyvim.plugins.extras.editor.flash" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
